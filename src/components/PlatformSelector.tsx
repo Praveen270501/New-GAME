08263cdd19ed01515b581a810cb95eb43feb8c6b
PRAVEENAT123 <praveenm@yectra.com>
2026-03-25 16:31:17 +0530

    Comimt

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,17 +1,17 @@
 import { Platform } from "@/hooks/useGames";
 import usePlatforms from "@/hooks/usePlatFortms";
-import { HStack, Menu, Text } from "@chakra-ui/react";
-import { Button, Portal } from "@chakra-ui/react";
+import { HStack, Menu, Text, Portal, Button } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import { Box } from "@chakra-ui/react";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
   selectedPlatform: Platform | null;
 }
+
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, errorMsg } = usePlatforms();
   if (errorMsg) return null;
+
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
@@ -32,13 +32,13 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
       <Portal>
         <Menu.Positioner>
           <Menu.Content>
-            {data.map((platforms) => (
+            {data.map((platform) => (
               <Menu.Item
-                key={platforms.id}
-                onClick={() => onSelectPlatform(platforms)}
-                value={platforms.name}
+                key={platform.id}
+                value={String(platform.id)}
+                onClick={() => onSelectPlatform(platform)}
               >
-                {platforms.name}
+                <Text>{platform.name}</Text>
               </Menu.Item>
             ))}
           </Menu.Content>
